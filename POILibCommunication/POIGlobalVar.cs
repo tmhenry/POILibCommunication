0ec2b1bdbbac662670ce0a1713713b1287e790ba
Henry Pengye Xia <xiapengye@gmail.com>
2013-07-18 23:01:54 +0800

    Add config file loading to improve deployment efficiency

--- POILibCommunication/POIGlobalVar.cs
+++ POILibCommunication/POIGlobalVar.cs
@@ -6,6 +6,9 @@ using System.Text;
 using System.Windows;
 using System.Diagnostics;
 
+using System.IO;
+using System.Web.Script.Serialization;
+
 namespace POILibCommunication
 {
     //Define all the global variables here
@@ -17,8 +20,8 @@ namespace POILibCommunication
         public static Dictionary<string, POIUser> WebUserProfiles { get; set; }
         public static Dictionary<string, POIUser> WebConUserMap { get; set; }
 
-        public static String ContentServerHome { get { return "http://192.168.0.130/POIWebService/contentServer/"; } }
-        public static String DNSServerHome { get { return "http://192.168.0.130/POIWebService/dnsServer/interface.php"; } }
+        public static String ContentServerHome { get; set; }
+        public static String DNSServerHome { get; set; }
 
         public static String KeywordsFileName { get { return "POI_Keywords.txt";} }
         public static String KeywordsFileType { get { return ".txt"; } }
@@ -29,7 +32,28 @@ namespace POILibCommunication
 
         public static void POIDebugLog(object msg)
         {
-            Debug.WriteLine(msg);
+            //Debug.WriteLine(msg);
+        }
+
+        public static void LoadConfigFile()
+        {
+            string fn = Path.Combine(Directory.GetCurrentDirectory(), @"poi_config");
+
+            try
+            {
+                string configJson = File.ReadAllText(fn);
+                JavaScriptSerializer jsonHandler = new JavaScriptSerializer();
+                Dictionary<string, string> configDict = jsonHandler.Deserialize(configJson, typeof(Dictionary<string, string>)) 
+                    as Dictionary<string, string>;
+
+                ContentServerHome = configDict["ContentServer"];
+                DNSServerHome = configDict["DNSServer"];
+
+            }
+            catch (Exception e)
+            {
+                POIDebugLog(e);
+            }
         }
     }
 }
